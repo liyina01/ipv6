ff108b0932076c91ed7534fec9e95b2060c4c34b
liyina01 <127083727+liyina01@users.noreply.github.com>
2023-03-06 15:09:11 +0800

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -116,8 +116,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=12000
-LAST_PORT=13000
+FIRST_PORT=10000
+LAST_PORT=11999
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
